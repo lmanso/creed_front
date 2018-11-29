0c65cfde01b476bb0ca3fca4e3241aebfe550e75
lmanso <30408665+lmanso@users.noreply.github.com>
2018-11-29 16:37:06 +0000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
+<div class="content">
+<div class="banner no-padding">
+    <h1 class="text-center white">Bonjour</h1>      
+    <p class="text-center white">Ici vous pourrez trouver les assasins</p>
+</div>
+</div>
+@endsection
